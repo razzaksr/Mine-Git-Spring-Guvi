a728d7b5769a33007467c1e64b944d0293b39365
razzaksr <razzaksr@gmail.com>
2024-10-07 14:01:05 +0530

    Spring boot app created

---
+++ src/main/java/com/example/mine_spring_crud/MineSpringCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.mine_spring_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MineSpringCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MineSpringCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mine_spring_crud/MineSpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mine_spring_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MineSpringCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
